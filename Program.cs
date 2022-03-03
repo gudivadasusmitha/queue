7f9e0e4c7cb9948e959070aafeb9b4960fa1f224
gudivadasusmitha <Gudivada.Susmitha@ibm.com>
2022-03-03 11:48:47 +0530

    Add project files.

---
+++ Program.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace queue
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            var value = new Queue<string>();
+            value.Enqueue("ap");
+            value.Enqueue("delhi");
+            value.Enqueue("punjab");
+            Console.Write("total number of elemts in the queue are :");
+            Console.WriteLine(value.Count);
+            Console.ReadLine();
+        }
+    }
+}
